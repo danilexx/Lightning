f6bc7601b60923b9717dc91314727d3b50e81d52
SuchACharles <48660364+SuchACharles@users.noreply.github.com>
2020-02-29 15:44:27 -0800

    Update Custom.js

--- src/components/Lightning/Custom.js
+++ src/components/Lightning/Custom.js
@@ -1,9 +1,33 @@
 export default {
     name: 'Custom',
+    data: function () {
+        return {
+            modal: false
+        }
+    },
     template: `
 <div>
 
-<div class="title">Custom</div>
+    <a v-on:click="modal = !modal" v-bind:class="{ 'is-active': modal }">Custom</a>
+
+    <transition name="fade">
+      <div v-if="modal" class="modal" v-bind:class="{ 'is-active': modal }">
+        <div v-on:click="modal = !modal" class="modal-background"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Lightning</p>
+            <button v-on:click="modal = !modal" class="delete" aria-label="close"></button>
+          </header>
+          <section class="modal-card-body">
+            <modalContent />
+          </section>
+          <footer class="modal-card-foot">
+            <!-- <button class="button is-success">Save changes</button> -->
+            <button v-on:click="modal = !modal" class="button">Close</button>
+          </footer>
+        </div>
+      </div>
+    </transition>
 
 </div>
     `
