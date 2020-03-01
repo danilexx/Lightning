5d8f47e581672b824e69b40cc244cf07cda4d146
SuchACharles <48660364+SuchACharles@users.noreply.github.com>
2020-02-29 16:33:04 -0800

    Update Custom.js

--- src/components/Lightning/Custom.js
+++ src/components/Lightning/Custom.js
@@ -1,14 +1,31 @@
 export default {
-    name: 'Custom',
-    data: function () {
-        return {
-            modal: false
-        }
-    },
-    template: `
+  name: 'Custom',
+  data: function () {
+    return {
+      modal: false,
+      port: ''
+    }
+  },
+  mounted() {
+    if (localStorage.port) {
+      this.port = localStorage.port;
+    }
+  },
+  watch: {
+    port(newPort) {
+      localStorage.port = newPort;
+    }
+  },
+  template: `
 <div>
 
-    <a v-on:click="modal = !modal" v-bind:class="{ 'is-active': modal }">Custom</a>
+    <a v-on:click="modal = !modal" v-bind:class="{ 'is-active': modal }" class="is-pulled-right">
+      <span class="tag is-primary">
+        <span class="icon">
+          <i class="fas fa-pen"></i>
+        </span>
+      </span>
+    </a>
 
     <transition name="fade">
       <div v-if="modal" class="modal" v-bind:class="{ 'is-active': modal }">
@@ -20,7 +37,23 @@ export default {
           </header>
 
           <section class="modal-card-body">
-            Content
+
+            <div class="field">
+              <label class="label">Name</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="Name">
+              </div>
+              <p class="help">Enter the name of your port.</p>
+            </div>
+
+            <div class="field">
+              <label class="label">Port</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="Port" v-model="port">
+              </div>
+              <p class="help">Enter the number of your port.</p>
+            </div>
+
           </section>
 
           <footer class="modal-card-foot">
@@ -29,6 +62,14 @@ export default {
         </div>
       </div>
     </transition>
+
+    <div class="media-content">
+    <div class="content">
+        <span class="tag is-primary">{{ port }}</span>
+        <div class="title">Custom</div>
+        <button class="button" onclick="lightning('Custom', localStorage.getItem('port'));">Port</button>
+    </div>
+    </div>
 
 </div>
     `
