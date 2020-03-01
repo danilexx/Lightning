e89a953c36ec222eb95038a7105976c55e422f76
SuchACharles <48660364+SuchACharles@users.noreply.github.com>
2020-02-29 16:35:46 -0800

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,44 +10,25 @@ export default {
 
 <section class="section">
     <div class="columns is-multiline">
+
         <div class="column is-half">
-        <div class="box">
-            <div class="media-content">
-            <div class="content">
-                <span class="tag is-primary">25565</span>
-                <div class="title">Minecraft</div>
-                <button class="button" onclick="lightning('Minecraft', 25565);">Port</button>
-            </div>
+            <div class="box">
+                <div class="media-content">
+                    <div class="content">
+                        <span class="tag is-primary">25565</span>
+                        <div class="title">Minecraft</div>
+                        <button class="button" onclick="lightning('Minecraft', 25565);">Port</button>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
+
         <div class="column is-half">
-        <div class="box">
-            <div class="media-content">
-            <div class="content">
-                <span class="tag is-primary">28960</span>
-                <div class="title">Call of Duty: World at War</div>
-                <button class="button" onclick="lightning('Call of Duty: World at War', 28960);">Port</button>
-            </div>
+            <div class="box">
+                <Custom />
             </div>
         </div>
-        </div>
-        <div class="column is-half">
-        <div class="box">
-            <div class="media-content">
-            <div class="content">
-                <span class="tag is-primary">3074</span>
-                <div class="title">Grand Theft Auto IV</div>
-                <button class="button" onclick="lightning('Grand Theft Auto IV', 3074);">Port</button>
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="column is-half">
-        <div class="box">
-            <Custom />
-        </div>
-        </div>
+
     </div>
 </section>
 
