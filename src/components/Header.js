6fdcf4a35ff5819d5547e841e5062b4a8965f566
SuchACharles <48660364+SuchACharles@users.noreply.github.com>
2020-06-04 20:08:30 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default {
   <div class="hero-body">
     <div class="container">
       <h1 class="title">Lightning</h1>
-      <h2 class="subtitle">A port forwarding utility. Created by SuchACharles.</h2>
+      <h2 class="subtitle">A port forwarding utility.</h2>
     </div>
   </div>
 </section>
